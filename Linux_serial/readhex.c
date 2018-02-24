5c518663b98b247730a744bebbd88fa60186e566
AEAstro <dakotapb2012@gmail.com>
2018-02-24 00:11:53 +0000

    We added a logging feature to readhex to capture raw ADS-B or IMU data.

--- Linux_serial/readhex.c
+++ Linux_serial/readhex.c
@@ -3,18 +3,22 @@
 
 void main(int argc, char** argv)
 {
-    FILE *fp;
+    FILE *fp, *logfp;
     unsigned char buffer[80];
 
-    if(argc < 2)
+    if (argc == 3)
     {
-        printf("Will use ttyUSB0\n");
-        fp=fopen("/dev/ttyUSB0", "r");
+        printf("Will read from %s\n", argv[1]);
+        fp=fopen(argv[1], "r");
+        printf("Will log to %s\n", argv[2]);
+        logfp=fopen(argv[2], "w");
     }
     else
     {
-        printf("Will use %s\n", argv[1]);
-        fp=fopen(argv[1], "r");
+        printf("Will use ttyUSB0\n");
+        fp=fopen("/dev/ttyUSB0", "r");
+        printf("Will use ADSB_log.bin\n");
+        logfp=fopen("ADSB_log.bin", "w");
     }
 
     if(fp == (FILE *)0)
@@ -22,10 +26,20 @@ void main(int argc, char** argv)
         perror("opening device or ttyUSB0");  
         exit(-1);
     }
+
+    if(logfp == (FILE *)0)
+    {
+        perror("opening log file");  
+        exit(-1);
+    }
    
     while(1)
     {
         fread(buffer, 1, 1, fp);
         printf("%X", buffer[0]);
+        fwrite(buffer, 1, 1, logfp); 
     } 
+
+    fclose(fp);
+    fclose(logfp);
 }
